9eb152875cbd6b0072feed0f8c0369c6b751d557
Thulasi Ram <thulasiramchilukoti0301@gmail.com>
2026-02-03 08:36:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (100.00%)

---
+++ 0070-climbing-stairs/solution.c
@@ -0,0 +1,14 @@
+int climbStairs(int n) {
+    if(n<=2){
+        return n;
+    }
+    int prev2 = 1;
+    int prev1 = 2;
+    int curr;
+    for(int i=3; i<=n;i++){
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return curr;
+}
