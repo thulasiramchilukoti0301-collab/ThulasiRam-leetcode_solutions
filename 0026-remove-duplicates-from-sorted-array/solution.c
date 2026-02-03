0fa5753b693b5873f84aa8bf722b4fbad4634fe0
Thulasi Ram <thulasiramchilukoti0301@gmail.com>
2026-02-03 04:56:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.2 MB (100.00%)

---
+++ 0026-remove-duplicates-from-sorted-array/solution.c
@@ -0,0 +1,14 @@
+int removeDuplicates(int* nums, int numsSize) {
+  if(numsSize == 0){
+    return 0;
+  }
+  int j = 0;
+  for(int i = 1;i<numsSize; i++){
+    if(nums[j] != nums[i]){
+        j++;
+        nums[j] = nums[i];
+    }
+  }
+  return j+1;
+  }  
+
