2b91f1482bbf1ab559604fe5fc59294a3017a51f
Thulasi Ram <thulasiramchilukoti0301@gmail.com>
2026-02-04 18:38:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (100.00%)

---
+++ 0029-divide-two-integers/solution.c
@@ -0,0 +1,21 @@
+#include<limits.h>
+#include<stdlib.h>
+int divide(int dividend, int divisor) {
+    if(dividend == INT_MIN && divisor == -1)
+        return INT_MAX;
+    int negative = (dividend < 0)^(divisor < 0);
+    long long a = llabs((long long)dividend);
+    long long b = llabs((long long)divisor);
+    long long result = 0;
+    while(a>=b){
+        long long temp = b;
+        long long multiple = 1;
+        while((temp << 1) <= a){
+            temp <<= 1;
+            multiple <<= 1;
+        }
+        a -= temp;
+        result += multiple;
+    }
+    return negative ? -result : result;
+}
