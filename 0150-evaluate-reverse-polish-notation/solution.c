f5d89eca39aefc8922bd4ca6e432b687d98203ea
Thulasi Ram <thulasiramchilukoti0301@gmail.com>
2026-03-28 19:18:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.4 MB (43.60%)

---
+++ 0150-evaluate-reverse-polish-notation/solution.c
@@ -0,0 +1,30 @@
+int evalRPN(char** tokens, int tokensSize) {
+    int stack[tokensSize];
+    int top = -1;
+    for(int i = 0;i < tokensSize;i++){
+        if(strcmp(tokens[i],"+") == 0){
+            int b = stack[top--];
+            int a = stack[top--];
+            stack[++top] = a+b;
+        }
+        else if(strcmp(tokens[i],"-") == 0){
+            int b = stack[top--];
+            int a = stack[top--];
+            stack[++top] = a-b;
+        }
+        else if(strcmp(tokens[i],"*") == 0){
+            int b = stack[top--];
+            int a = stack[top--];
+            stack[++top] = a*b;
+        }else if(strcmp(tokens[i],"/") == 0){
+            int b = stack[top--];
+            int a = stack[top--];
+            stack[++top] = a/b;
+        }
+        else {
+            stack[++top] = atoi(tokens[i]);
+        } 
+    }
+    return stack[top];
+    
+}
